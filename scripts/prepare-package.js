de94e1ffa4107ad8919972fae7a8e03b5b3d998d
DioNanos <noreply@github.com>
2026-04-06 14:38:18 +0200

    fix: add @mmmbuto/node-pty-android-arm64 to prepare-package.js optionalDependencies

--- scripts/prepare-package.js
+++ scripts/prepare-package.js
@@ -175,6 +175,8 @@ const distPackageJson = {
     '@lydell/node-pty-linux-x64': '1.2.0-beta.10',
     '@lydell/node-pty-win32-arm64': '1.2.0-beta.10',
     '@lydell/node-pty-win32-x64': '1.2.0-beta.10',
+    // TERMUX PATCH: Android ARM64 PTY support
+    '@mmmbuto/node-pty-android-arm64': '1.1.0',
     '@teddyzhu/clipboard': '0.0.5',
     '@teddyzhu/clipboard-darwin-arm64': '0.0.5',
     '@teddyzhu/clipboard-darwin-x64': '0.0.5',
